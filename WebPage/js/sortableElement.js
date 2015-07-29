7d6156006cb8bf30ce3a9f97cb6742e5ba995d73
andDiFolca <and.difolca@stud.uniroma3.it>
2015-07-29 11:27:29 +0200

    facendo sti bottoni

--- WebPage/js/sortableElement.js
+++ WebPage/js/sortableElement.js
@@ -1,21 +1,38 @@
 //sfrutta massicciamente jquery()
 
 function makeSelection (toMatchArray){
-	$('#chartcontainer').append($('<div></div>')).attr({
-        id: 'sortableSelector'
+	$('#chartcontainer').append($('<div></div>').attr({
+        id: 'sortableSelectorDiv'
     }));
 	
-	$('#sortableSelector').append($('<select></select>')).attr({
+	$('#sortableSelectorDiv').append($('<select></select>').attr({
 		id:'selector',
 		class: 'selectpicker',
 		multiple,
 		data-max-options:"5"
-	});
+	}));
 
 	toMatchArray.forEach( function (elem){
 		var stringToAppend = '<option>'+elem.description+'</option>'
 		$('#selector').append(stringToAppend);
 	});
 
+	$('#sortableSelectorDiv').append($('<ul></ul>').attr({
+		id: 'sortable',
+	}));
 	
+	$('#sortable').sortable();
+	$("#sortable").disableSelection();
+
+	$( "#selector" ).change(function () {
+		var selectedValue;
+    	$( "#selector option:selected" ).each(function() {
+			selectedValue.push($( this ).text());	
+    	});
+    	$('#sortable:option').remove();
+    	selectedValue.forEach(function (elem){
+    		var stringToAppend = '<li>'+elem+'</li>'
+    		$('#sortable').append(stringToAppend);
+    	});
+	});
 }
