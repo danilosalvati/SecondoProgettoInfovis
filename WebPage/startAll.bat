2d8da77e5ec5766100205094abd6be392d64229f
andDiFolca <and.difolca@stud.uniroma3.it>
2015-08-03 23:12:29 +0200

    aggiunto il .bat non testato

---
+++ WebPage/startAll.bat
@@ -0,0 +1,3 @@
+cd %0
+echo checkout your browser at localhost:8000
+python -m SimpleHTTPServer 8000
--- WebPage/startAll.sh
+++ WebPage/startAll.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd /
+cd $DIR
 python -mwebbrowser http://localhost:8000
 echo "checkout your browser at localhost:8000"
 python -m SimpleHTTPServer 8000
